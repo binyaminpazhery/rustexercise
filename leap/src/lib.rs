743d6424a245348770b798df75bea5e9c77f7613
binyaminpazhery <58895740+binyaminpazhery@users.noreply.github.com>
2020-05-22 11:32:30 +0530

    leap exercise

---
+++ leap/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn is_leap_year(year: u64) -> bool {
+	let mut is_leap = true;
+	if year%4 ==0 {
+		if year%100==0{
+			if year%400==0 {
+				println!("{} is leap year",year);
+			}else {
+				is_leap = false;
+				println!("{} is no t a leap year", year);
+			}
+		}else {
+			println!("{} is not a leap year", year);
+		}
+	}else {
+		is_leap=false;
+		println!("{} is not a leap year", year );
+	}
+    is_leap
+}
